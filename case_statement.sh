5ded589265b27bad7acfca63bccf3d7a069a9af3
MikeTemi <michaelsiwoku@gmail.com>
2025-05-04 16:40:47 +0100

    case statements in bash

---
+++ case_statement.sh
@@ -0,0 +1,22 @@
+job = "DevOps Engineer"
+
+case $job in
+	"Software Engineer")
+		echo "I think the industry has too many of this. Not a bad role, if the project is exciting"
+		;;
+	"ML/AI Engineer")
+		echo "Now, this is a job I would absolutely love!"
+		;;
+	"Cloud Engineer")
+		echo "It's a skill I am looking forward to learn anyway."
+		;;
+	"CyberSecurity Engineer")
+		echo "Sounds so badass!!!"
+		;;
+	"DevOps Engineer")
+		echo "My current track. Hope I get a job soon!"
+		;;
+	*)
+		echo "I really need a job anyways, I need one urgently, LOL!"
+		;;
+esac
